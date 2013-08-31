a131dcfc5b663f7ec00101488d9d2306af9eda6d
ilsaul <ilsaul2@gmail.com>
2013-08-31 10:49:57 +0200

    change permission on the files for online editing and some italian language improvement

--- languages/it/ad_lang_home.php
+++ languages/it/ad_lang_home.php
@@ -9,25 +9,31 @@
 
 $lang = array(
 
-'button_save_notes' => 'Save Notepad',
-'alert_install_locked' => 'Your <i>install</i> directory still exists.  While the installer is locked, we recommend deleting or renaming the <i>install</i> directory for added security.',
-'alert_install_dir_exists' => 'Your <i>install</i> directory still exists.  We strongly recommend deleting or renaming this directory for added security.',
-'system_status' => 'System Status',
-'product_version' => 'Product Version',
-'php_version' => 'PHP Version',
-'mysql_version' => 'MySQL Version',
-'version_check' => 'Version Check',
-'admin_notepad' => 'Admin Notepad',
-'common_acp_sections' => 'Common ACP Sections',
-'recent_tickets' => 'Recent Tickets',
-'recent_acp_actions' => 'Recent ACP Actions',
+'alert_install_locked' => 'La tua directory di <i>installazione</i> &egrave; ancora presente. Anche se l\'installazione &egrave; bloccata, raccomandiamo di cancellare o rinominare la directory di <i>installazzione</i> per aumentare la sicurezza.',
+'alert_install_dir_exists' => 'La tua directory di <i>installazione</i> &egrave; ancora presente. Raccomandiamo caldamente di cancellare o rinominare la directory di <i>installazzione</i> per aumentare la sicurezza.',
+
+'system_status' => 'Stato del Sistema',
+'version_check' => 'Ultimo controllo della versione',
+'product_version' => 'Versione del prodotto',
+'php_version' => 'Versione di PHP',
+'mysql_version' => 'Versione di MySQL',
+
+'button_save_notes' => 'Salva le Note',
+'admin_notepad' => 'Note dell\'amministratore',
+
+'common_acp_sections' => 'Sezioni principali del ACP',
+'recent_tickets' => 'Ticket recenti',
+'recent_acp_actions' => 'Recenti azioni in ACP',
 'skins' => 'Skins',
-'settings' => 'Settings',
-'languages' => 'Languages',
-'users' => 'Users',
-'kb_articles' => 'KB Articles',
-'documentation' => 'Documentation',
-'check_now' => 'Check Now',
+'settings' => 'Imostazioni',
+'languages' => 'Lingue',
+'users' => 'Utenti',
+'kb_articles' => 'Documenti',
+'documentation' => 'Documentazione',
+'check_now' => 'Controlla Adesso',
+
+'td_latest_news' => 'Ultime Notizie',
+'td_latest_news_msg' => 'TODO',
 
 );
 
